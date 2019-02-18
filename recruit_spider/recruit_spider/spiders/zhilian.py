d710aab3e0da2c56414fd68bb4fc496bcf728346
dhiuwha <zhangbohao@ichangtou.com>
2019-02-18 16:53:32 +0800

    xpath 优化

--- recruit_spider/recruit_spider/spiders/zhilian.py
+++ recruit_spider/recruit_spider/spiders/zhilian.py
@@ -11,9 +11,10 @@ class ZhilianSpider(scrapy.Spider):
     start_urls = ['https://fe-api.zhaopin.com/c/i/sou?pageSize=90&cityId=538&workExperience=-1&education=-1&companyType=-1&employmentType=-1&jobWelfareTag=-1&kw=python&kt=3']
 
     def parse(self, response):
-        item = ZhilianSpiderItem()
+
         data = json.loads(response.body.decode('utf8'))
         for element in data['data']['results']:
+            item = ZhilianSpiderItem()
             item['position_name'] = element['jobName']
             item['position_url'] = element['positionURL']
             item['company_name'] = element['company']['name']
@@ -26,21 +27,21 @@ class ZhilianSpider(scrapy.Spider):
             item['update_time'] = element['updateDate']
             item['end_time'] = element['endDate']
             item['header_count'] = element['recruitCount']
+            # if item['position_url'] == 'https://jobs.zhaopin.com/CC263265337J00086662006.htm':
             yield scrapy.Request(url=item['position_url'], meta={"item": item}, callback=self.detail_parse, dont_filter=True)
 
     def detail_parse(self, response):
         item = response.meta['item']
         item['position_detail_info'] = self.get_position_detail_info(response)
-        print(item)
         yield item
 
     @staticmethod
     def get_position_detail_info(position):
-        content = position.xpath('//div[@class="responsibility pos-common"]/div[@class="pos-ul"]/div/text()').re('[^\xa0]*')
-        if len(content) == 0:
-            print("enter")
-            content = position.xpath('//div[@class="responsibility pos-common"]/div[@class="pos-ul"]/p/text()').re('[^\xa0]*')
-        # for sentence in content:
-        #     if sentence == '' or sentence == ' ':
-        #         content.remove(sentence)
+        content = position.xpath(
+            '//div[@class="responsibility pos-common"]/div[@class="pos-ul"]/text()').re('[^\xa0\s]*')
+        content.extend(position.xpath(
+                '//div[@class="responsibility pos-common"]/div[@class="pos-ul"]/descendant::*/text()').re('[^\xa0]*'))
+        for sentence in content:
+            if sentence == '' or sentence == ' ':
+                content.remove(sentence)
         return content
