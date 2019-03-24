2d0b7b6577c3e3ac4897b7ac5b28a4d940424081
dhiuwha <wxzhangbh@gmail.com>
2019-03-24 20:52:43 +0800

    构造链接插入redis

--- url_structure/dao/redis_insert.go
+++ url_structure/dao/redis_insert.go
@@ -4,14 +4,15 @@ import (
 	"../config"
 	"fmt"
 	"github.com/garyburd/redigo/redis"
+	"strconv"
 )
 
 func RedisInsert(platform, city []string, keyword string) {
-	c, _ := redis.Dial("tcp", config.RedisHost+":"+config.RedisPort)
+	c, _ := redis.Dial("tcp", config.RedisHost+":"+config.RedisPort, redis.DialDatabase(1))
 	construct := ConstructUrl(platform, city, keyword)
 	for k, v := range construct {
 		for _, url := range v {
-			_, _ = c.Do("sadd", k, url)
+			_, _ = c.Do("lpush", k, url)
 		}
 	}
 }
@@ -34,9 +35,9 @@ func ConstructUrl(platform, city []string, keyword string) map[string][]string {
 		for _, c := range city {
 			_, judge := result[k[plat]]
 			if judge {
-				result[k[plat]] = make([]string, 0, 5)
+				result[k[plat]] = make([]string, 1)
 			}
-			result[k[plat]] = append(result[k[plat]], p[plat](c, keyword))
+			result[k[plat]] = append(result[k[plat]], p[plat](c, keyword)...)
 			fmt.Println(p, c)
 		}
 	}
@@ -44,20 +45,66 @@ func ConstructUrl(platform, city []string, keyword string) map[string][]string {
 	return result
 }
 
-type F func(city, keyword string) string
+type F func(city, keyword string) []string
 
-func ConstructLagou(city, keyword string) string {
-	return "lagou"
+func ConstructLagou(city, keyword string) []string {
+	result := make([]string, 1)
+	for i := 0; i < 5; i++ {
+		print()
+		result = append(result, "https://www.lagou.com/jobs/list_"+keyword+"?labelWords=&fromSearch=true&suginput="+
+			"{\"city\":\""+city+"\", \"keyword\":\""+keyword+"\",\"page\": \""+strconv.Itoa(i+1)+"\"}")
+	}
+
+	return result
 }
 
-func ConstructBoss(city, keyword string) string {
-	return "boss"
+func ConstructBoss(city, keyword string) []string {
+	result := make([]string, 1)
+	cityMap := map[string]string{
+		"北京": "101010100",
+		"上海": "101020100",
+		"广州": "101280100",
+		"深圳": "101280600",
+	}
+	for i := 0; i < 5; i++ {
+		print()
+		result = append(result, "https://www.zhipin.com/c"+cityMap[city]+"/?query="+keyword+"&page="+strconv.Itoa(i+1)+"&ka=page-"+strconv.Itoa(i+1)+
+			"{\"city\":\""+city+"\", \"keyword\":\""+keyword+"\"}")
+	}
+
+	return result
 }
 
-func ConstructZhilian(city, keyword string) string {
-	return "zhilian"
+func ConstructZhilian(city, keyword string) []string {
+	result := make([]string, 1)
+	cityMap := map[string]string{
+		"北京": "530",
+		"上海": "538",
+		"广州": "763",
+		"深圳": "765",
+	}
+	for i := 0; i < 5; i++ {
+		print()
+		result = append(result, "https://fe-api.zhaopin.com/c/i/sou?start="+strconv.Itoa(i*90)+"&pageSize=90&cityId="+cityMap[city]+"&kw="+keyword+"&kt=3"+
+			"{\"city\":\""+city+"\", \"keyword\":\""+keyword+"\"}")
+	}
+
+	return result
 }
 
-func Construct51(city, keyword string) string {
-	return "51"
+func Construct51(city, keyword string) []string {
+	result := make([]string, 1)
+	cityMap := map[string]string{
+		"北京": "010000,000000,0000,00",
+		"上海": "020000,000000,0000,00",
+		"广州": "030200,000000,0000,00",
+		"深圳": "040000,000000,0000,00",
+	}
+	for i := 0; i < 5; i++ {
+		print()
+		result = append(result, "https://search.51job.com/list/"+cityMap[city]+",9,99,"+keyword+",2,"+strconv.Itoa(i+1)+".html"+
+			"{\"city\":\""+city+"\", \"keyword\":\""+keyword+"\"}")
+	}
+
+	return result
 }
--- url_structure/main/insert.go
+++ url_structure/main/insert.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"../dao"
-	"fmt"
 )
 
 func main() {
-	dao.RedisInsert([]string{"拉勾"}, []string{"b"}, "c")
-	for k, v := range map[string]string{"a": "1", "b": "2"} {
-		fmt.Println(k, v)
-	}
+	dao.RedisInsert([]string{"拉勾"}, []string{"广州"}, "机器学习")
+	//for k, v := range map[string]string{"a": "1", "b": "2"} {
+	//	fmt.Println(k, v)
+	//}
 }
