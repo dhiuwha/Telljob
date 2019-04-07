b28fe30b4157e77970501bae3e7e3386ed04b8f0
dhiuwha <wxzhangbh@gmail.com>
2019-04-07 16:37:51 +0800

    微调

---
+++ url_structure/config/config.go
@@ -0,0 +1,7 @@
+package config
+
+const RedisHost = "database"
+const RedisPort = "9736"
+
+const MongoDBHost = "database"
+const MongoDBPort = "27017"
--- url_structure/main/insert.go
+++ url_structure/main/insert.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"../dao"
+	"../process"
 )
 
 func main() {
-	dao.RedisInsert([]string{"拉勾"}, []string{"广州"}, "机器学习")
+	process.RedisInsert([]string{"拉勾", "智联", "直聘", "51"}, []string{"广州", "深圳", "上海", "北京"}, "java")
 	//for k, v := range map[string]string{"a": "1", "b": "2"} {
 	//	fmt.Println(k, v)
 	//}
